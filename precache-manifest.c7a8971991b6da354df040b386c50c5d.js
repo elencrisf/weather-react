8f333990a64491c1c37309ed5dde0ba9d8412845
elencrisf <elen.crisf@uol.com.br>
2020-08-14 10:50:20 -0700

    Updates

---
+++ precache-manifest.c7a8971991b6da354df040b386c50c5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835a67fece6e6bec13ae3471bc795876",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "fa3f666fc55c28562968",
+    "url": "/weather-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "55d4ee930998e2f72213",
+    "url": "/weather-app/static/css/main.241ead33.chunk.css"
+  },
+  {
+    "revision": "fa3f666fc55c28562968",
+    "url": "/weather-app/static/js/2.adf49309.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-app/static/js/2.adf49309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d4ee930998e2f72213",
+    "url": "/weather-app/static/js/main.8e79a04a.chunk.js"
+  },
+  {
+    "revision": "7e49862b5c1407e8424b",
+    "url": "/weather-app/static/js/runtime-main.c28fc006.js"
+  },
+  {
+    "revision": "0a108be00e1026d3b989c9a6729ffb44",
+    "url": "/weather-app/static/media/glow-blur.0a108be0.jpg"
+  }
+]);
